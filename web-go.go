ff18c677c761efc5113ce1f8b9ed0b6faf7fb83a
toluwase <toluwasethomas1@gmail.com>
2021-09-14 16:57:25 +0100

    hello

--- web-go.go
+++ web-go.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+)
+
+func handler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w,"Hello World")
+}
+
+func main()  {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+
+type T2 struct {}
+
+// T1 is deprecated, please use T2
+type T1 = T2
+
+//func main() {
+//	var t T1
+//	f(t)
+//}
+
+func f(t T1) {
+	// print main.T2
+	println(reflect.TypeOf(t).String())
+}
